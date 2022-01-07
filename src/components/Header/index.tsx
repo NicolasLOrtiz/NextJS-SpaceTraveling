b220e28b1b73f426b28adf4c2f1209fd3d4deb3f
NicolasLOrtiz <contato.nicolol@gmail.com>
2022-01-06 21:13:37 -0300

    Parte 1

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
-export default function Header() {
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
   // TODO
+  return (
+    <header className={styles.logo}>
+      <Link href="/">
+        <a>
+          <Image src="/logo.svg" alt="logo" width={238.62} height={25.63} />
+        </a>
+      </Link>
+    </header>
+  );
 }
